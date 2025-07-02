baba39402aa8f9d3b781a7cf94c23dcd7fdb2e87
YeshuaRodriguez-1 <yeshua@ucsb.edu>
2025-07-01 22:31:43 -0700

    Finished Lab 1 programs

---
+++ Lab1/calculate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+int var1, var2, var3;
+cout << "Please enter 3 numbers." << endl;
+cin >> var1 >> var2 >> var3;
+int result = 3 * (var1 + 2 * var2) - 4 * var3;
+cout << "They formula result is: " <<result << endl;
+
+return 0;
+}
+
