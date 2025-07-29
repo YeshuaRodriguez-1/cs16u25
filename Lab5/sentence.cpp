27db108f04bb6d5d7491bd988a2e085df4624765
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2025-07-29 15:06:26 -0700

    Add files via upload

---
+++ Lab5/sentence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+// MISSING FUNCTION DECLARATIONS HERE (you can remove these comments)
+
+int main()
+{
+    string sentence;
+
+    // MISSING CODE TO ASK USER FOR SENTENCE (you can remove these comments)
+
+
+
+    int removed = sort_string(sentence);
+    print_freq(sentence);
+    cout << "removed: " << removed << endl;
+
+    return 0;
+}
+
+// MISSING FUNCTION DEFINITIONS HERE (you can remove these comments)
+
